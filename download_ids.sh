02ce6a7ed3de22f8f6d9a59cf44e7847d0478377
tlahaie <thomas.lahaie@icloud.com>
2025-04-29 17:15:37 +0200

    extract archives

--- download_ids.sh
+++ download_ids.sh
@@ -1,12 +1,17 @@
 mkdir -p ids
 
+# ids peak (camera SDK) with ueye transport layer
+if [ ! -f ids/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb ]; then
+    curl https://fr.ids-imaging.com/files/downloads/ids-peak/software/linux-arm64/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb -o ids/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb
+fi
+
 # ids dependencies
 if [ ! -f ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz ]; then
     curl https://fr.ids-imaging.com/files/downloads/ids-software-suite/software/linux-arm64/ids-software-suite-linux-arm64-4.96.1-debian.tgz -o ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz
 fi
 
-
-# ids peak (camera SDK) with ueye transport layer
-if [ ! -f ids/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb ]; then
-    curl https://fr.ids-imaging.com/files/downloads/ids-peak/software/linux-arm64/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb -o ids/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb
+# extract archive
+if [ -f ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz ]; then
+    tar -xzf ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz -C ids
+    tar -xzf ids/ueye_4.96.1.2054_arm64.deb.tar.gz -C ids
 fi
