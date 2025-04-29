c9497184f84a1543a133d182ac36111417232d7e
tlahaie <thomas.lahaie@icloud.com>
2025-04-29 17:21:10 +0200

    update

--- download_ids.sh
+++ download_ids.sh
@@ -5,13 +5,9 @@ if [ ! -f ids/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb ]; then
     curl https://fr.ids-imaging.com/files/downloads/ids-peak/software/linux-arm64/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb -o ids/ids-peak-with-ueyetl_2.15.0.0-352_arm64.deb
 fi
 
-# ids dependencies
+# ids dependencies and extract archive
 if [ ! -f ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz ]; then
-    curl https://fr.ids-imaging.com/files/downloads/ids-software-suite/software/linux-arm64/ids-software-suite-linux-arm64-4.96.1-debian.tgz -o ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz
-fi
-
-# extract archive
-if [ -f ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz ]; then
-    tar -xzf ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz -C ids
-    tar -xzf ids/ueye_4.96.1.2054_arm64.deb.tar.gz -C ids
+    curl https://fr.ids-imaging.com/files/downloads/ids-software-suite/software/linux-arm64/ids-software-suite-linux-arm64-4.96.1-debian.tgz -o ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz \
+    && tar -xzvf ids/ids-software-suite-linux-arm64-4.96.1-debian.tgz -C ids \
+    && tar -xzvf ids/ueye_4.96.1.2054_arm64.deb.tar.gz -C ids
 fi
